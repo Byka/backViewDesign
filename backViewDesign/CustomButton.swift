0aba48a8419069a96a3848ec2b931d259312b62c
sbyka <sbyka@JNETs-Mac-mini.local>
2018-08-02 17:20:52 +0530

    Initial commit

---
+++ backViewDesign/CustomButton.swift
@@ -0,0 +1,21 @@
+//
+//  CustomButton.swift
+//  backViewDesign
+//
+//  Created by sbyka on 02/08/18.
+//  Copyright © 2018 sbyka. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
